d62792a11b109b84c23b9a90fd2d935fca2976b4
Dilara Özdemir <dilaraozdmr35@gmail.com>
2023-06-21 17:30:43 +0300

    Search, Dark mode, Header, Footer

--- template.js
+++ template.js
@@ -1,1 +1,26 @@
-alert("deneme")
+//Dark Mode
+
+let darkModu=()=>{
+    window.document.classList.toggle('dark-mode');
+}
+
+//input search
+$(document).ready(function(){
+const searchApi=["adana", "balıkesir","Çorum", "denizli", "edirne", "malatya"];
+$("#tags").autocomplete({
+        source:searchApi
+    });
+});
+
+
+//Footer
+let newDate=()=>{
+    const date=new Date().getFullYear();
+    // JS DOM 
+    document.getElementById("date_id").innerHTML = date;
+
+    //Jquery DOM
+    $("#date_id").html(date);
+}
+
+newDate();
